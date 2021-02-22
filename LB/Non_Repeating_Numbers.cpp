3fccc8c36e3c591cd63b7a468a6d4c16163f592b
anurag1504t <64521184+anurag1504t@users.noreply.github.com>
2021-02-22 13:32:23 +0530

    Non Repeating Numbers

---
+++ LB/Non_Repeating_Numbers.cpp
@@ -0,0 +1,29 @@
+/*
+  Non Repeating Numbers
+*/
+
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
+        vector<int> ans;
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<nums.size()-1;++i)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                i++;
+            }
+            else
+            {
+                ans.push_back(nums[i]);
+            }
+        }
+        if(nums[nums.size()-1]!=nums[nums.size()-2])
+        {
+            ans.push_back(nums[nums.size()-1]);
+        }
+        return ans;
+    }
+};
