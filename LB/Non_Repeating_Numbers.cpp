f6b281f465466071036175fbaabaca74c285bd77
anurag1504t <64521184+anurag1504t@users.noreply.github.com>
2021-02-22 13:57:22 +0530

    Update Non_Repeating_Numbers.cpp

--- LB/Non_Repeating_Numbers.cpp
+++ LB/Non_Repeating_Numbers.cpp
@@ -7,6 +7,34 @@ class Solution
 public:
     vector<int> singleNumber(vector<int> nums) 
     {
+        vector<int> ans(2);
+        int xor_nums = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            xor_nums^=nums[i];
+        }
+        int rightmost_setbit = xor_nums&(~(xor_nums-1));
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]&rightmost_setbit)
+            {
+                ans[0]^=nums[i];
+            }
+            else
+            {
+                ans[1]^=nums[i];
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+class Solution
+{
+public:
+    vector<int> singleNumber(vector<int> nums) 
+    {
         vector<int> ans;
         sort(nums.begin(), nums.end());
         for(int i=0;i<nums.size()-1;++i)
